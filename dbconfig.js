342b40e6d072dca9b02a96f3f4cf676d94c59bca
SMHrvoje <hs53166@fer.hr>
2023-11-09 11:42:28 +0100

    update config for production

--- dbconfig.js
+++ dbconfig.js
@@ -6,7 +6,7 @@ const config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    ssl:true
+    ssl: { rejectUnauthorized: false }
 };
 
 module.exports = new Pool(config);
